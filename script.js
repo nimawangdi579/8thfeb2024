4431b441adfc4926e1a61b3ca30bb7ff839c11a5
nimawangdi579 <wangdinima579@gmail.com>
2024-02-08 10:31:32 +0600

    8th feb i am going to propose her

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("proposeBtn").addEventListener("click", function () {
+  alert(
+    "I'm the luckiest person in the world to have you as my forever loved girlfriend!"
+  );
+});
+document.getElementById("rejectBtn").addEventListener("click", function () {
+  alert("Its okay,No worries!");
+});
